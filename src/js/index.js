17a583b9764cf51168730a9ac4fddf2348d352fa
grze2000 <programista333@gmail.com>
2021-02-09 14:37:01 +0100

    feat: showing pupup with login form

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-/* Here goes your JS code */
+document.addEventListener('DOMContentLoaded', (event) => {
+    const showPopupBtn = document.querySelector('#show-popup-form');
+    const popup = document.querySelector('.popup');
+    const closeBtn = document.querySelector('.popup__close-btn');
+
+    showPopupBtn.addEventListener('click', function() {
+        popup.classList.add('popup--open');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        popup.classList.remove('popup--open');
+    });
+});
